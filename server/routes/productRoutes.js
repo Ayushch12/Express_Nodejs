e39dbb31a4fbb3fe164d65a32e66477d6e8d7a9b
Ayush chalisee <ayushchalise12@gmail.com>
2023-06-25 09:44:40 +0200

    Updated

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -20,7 +20,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/create-product", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
